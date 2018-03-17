0b7fdd579f66bbaca0f897cc6e9586545ef66e58
Christopher Sardegna <github@reagentx.net>
2018-03-16 23:43:39 -0700

    Fix comments

--- scripts/28.py
+++ scripts/28.py
@@ -5,7 +5,7 @@ import pandas as pd
 url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
 data = np.genfromtxt(url, delimiter=',', dtype=[float, float, float, float, object])
 
-# Take the fourth column `row[4]` from each row, make a Python list, and convert to a NumPy array
+# Take an arbitrary column from each row, make a Python list, and convert to a NumPy array
 col = lambda x: np.array([row[x] for row in data])
 d = col(0)
 print(f'NumPy\tMean: {np.mean(d):.2f}, Median: {np.median(d):.2f}, Sigma: {np.std(d):.2f}')
---
+++ scripts/29.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+
+
+url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
+data = np.genfromtxt(url, delimiter=',', dtype=[float, float, float, float, object])
+
+# Take an arbitrary column from each row, make a Python list, and convert to a NumPy array
+col = lambda x: np.array([row[x] for row in data])
+d = col(0)
+
