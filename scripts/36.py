6061d917d7a7d090f2fa51edd992b292444dceba
Christopher Sardegna <github@reagentx.net>
2018-03-17 16:28:13 -0700

    Add pandas example

--- scripts/36.py
+++ scripts/36.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 
 url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
@@ -6,6 +7,12 @@ url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
 data = np.genfromtxt(url, delimiter=',', dtype='float', usecols=(0, 1, 2, 3))
 
 # Use advanced indexing to pick two columns and all rows
-np.corrcoef()
-pwcorr = np.corrcoef(data[..., 0], data[..., 2])
-print(pwcorr)
+corr = np.corrcoef(data[..., 0], data[..., 3])
+np.set_printoptions(precision=2)
+print(f'{corr[0, 1]:.3f}')
+
+# Do this the right way
+data_pd = pd.read_csv(url, names=['sepallength', 'sepalwidth', 'petallength', 'petalwidth', 'species'])
+
+corr_pd = data_pd['sepallength'].corr(other=data_pd['petalwidth'])
+print(f'{corr_pd:.3f}')
