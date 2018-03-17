7a718e0d9d20ff78de6636112339be2715520bb6
Christopher Sardegna <github@reagentx.net>
2018-03-16 23:54:10 -0700

    Add exercise

--- scripts/29.py
+++ scripts/29.py
@@ -8,4 +8,9 @@ data = np.genfromtxt(url, delimiter=',', dtype=[float, float, float, float, obje
 # Take an arbitrary column from each row, make a Python list, and convert to a NumPy array
 col = lambda x: np.array([row[x] for row in data])
 d = col(0)
+print(d[:5])
 
+# We want to normaize everything such that the max value is 1, so we use `np.max(d)` which extracts the largest number from each row of d
+d_norm = d / np.max(d)
+np.set_printoptions(precision=2)
+print(d_norm[:5])
