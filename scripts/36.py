89d848a8eb700e03bf3f17436aa039ad7369db6b
Christopher Sardegna <github@reagentx.net>
2018-03-17 16:32:40 -0700

    Add SciPy example

--- scripts/36.py
+++ scripts/36.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from scipy.stats.stats import pearsonr
 
 
 url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
@@ -9,10 +10,14 @@ data = np.genfromtxt(url, delimiter=',', dtype='float', usecols=(0, 1, 2, 3))
 # Use advanced indexing to pick two columns and all rows
 corr = np.corrcoef(data[..., 0], data[..., 3])
 np.set_printoptions(precision=2)
-print(f'{corr[0, 1]:.3f}')
+print(f'NumPy:\t{corr[0, 1]:.3f}')
 
 # Do this the right way
 data_pd = pd.read_csv(url, names=['sepallength', 'sepalwidth', 'petallength', 'petalwidth', 'species'])
 
 corr_pd = data_pd['sepallength'].corr(other=data_pd['petalwidth'])
-print(f'{corr_pd:.3f}')
+print(f'Pandas:\t{corr_pd:.3f}')
+
+# Another right way
+corr_sp = pearsonr(data_pd['sepallength'], data_pd['petalwidth'])
+print(f'SciPy:\t{corr_sp[0]:.3f}, p = {corr_sp[1]:.3f}')
