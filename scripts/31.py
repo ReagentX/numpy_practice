d05a5383f46ffbf25f53fddd4d586f54415b6cfa
Christopher Sardegna <github@reagentx.net>
2018-03-17 14:55:01 -0700

    Add pandas example

--- scripts/31.py
+++ scripts/31.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 
 url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
@@ -11,4 +12,11 @@ print(d[:5])
 
 # Get the 10th and 95th percentiles
 d_percentiles = np.percentile(d, [10, 95])
+np.set_printoptions(precision=2)
 print(d_percentiles)
+
+# Do this the right way
+data_pd = pd.read_csv(url, names=['sepallength', 'sepalwidth', 'petallength', 'petalwidth', 'species'])
+d_pd = data_pd['sepallength']
+
+print(f'[{d_pd.quantile(0.1):.2f} {d_pd.quantile(0.95):.2f}]')
