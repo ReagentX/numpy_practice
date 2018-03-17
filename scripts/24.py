b5d6d20f6ad48880a0c131145b23364ecdec664f
Christopher Sardegna <github@reagentx.net>
2018-03-16 23:17:07 -0700

    Add new exercise and related data

--- scripts/24.py
+++ scripts/24.py
@@ -1,8 +1,15 @@
 import numpy as np
+import pandas as pd
 
 
-a = np.arange(9999)
+url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
 
-# Set the threshold with which to cut off arrays to infinity
-np.set_printoptions(threshold=np.inf)
-print(a)
+# We have to use a custom `dtype` to retain all of the data without losing any of the different formats, which means each row of data becomes a tuple
+print('Downloading data...')
+data = np.genfromtxt(url, delimiter=',', dtype=[float, float, float, float, object])
+print('First 5 rows:')
+print(data)
+
+# The right way to do this
+data_pd = pd.read_csv(url, names=['sepallength', 'sepalwidth', 'petallength', 'petalwidth', 'species'])
+print(data_pd.head())
