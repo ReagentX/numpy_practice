e673853679c9f74d4e03b0416ef7a11482050c5c
Christopher Sardegna <github@reagentx.net>
2018-03-17 16:10:43 -0700

    Add exercise

---
+++ scripts/35.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
+# Ignore the text column since it is pesky
+data = np.genfromtxt(url, delimiter=',', dtype='float', usecols=(0, 1, 2, 3))
+
+row, col = np.where(data)
+# Replace 120 random [row, col] pairs with `np.nan`
+data[np.random.choice((row), 120), np.random.choice((col), 120)] = np.nan
+
+# Find out the indexes of all of the `np.nan`s as [row, col]
+nans = np.argwhere(np.isnan(data))
+
+# Delete all of the rows in data, specify `axis=0` so we do not flatten and instead work on the rows separately
+data_clean = np.delete(data, nans[..., 0], axis=0)
+print(data_clean[:5])
