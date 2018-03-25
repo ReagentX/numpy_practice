b44ad5f57b84c5b487cb5f7e8383b9f1c903535f
Christopher Sardegna <github@reagentx.net>
2018-03-24 20:04:49 -0700

    Add pandas example

--- scripts/43.py
+++ scripts/43.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 
 url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
@@ -8,7 +9,24 @@ data = np.genfromtxt(url, delimiter=',', dtype='object')
 
 # The b'' means this is a byte literal
 data_filtered = np.sort(data[data[..., 4] == b'Iris-setosa', [2]].astype(float))
+print('NumPy')
 print(f'Overall: \t{data_filtered[-2]}')
 
 # Since the exercise uses the secnd largest unique value
 print(f'Unique: \t{np.unique(data_filtered)[-2]}')
+
+# Pandas example
+data_pd = pd.read_csv(url, names=['sepallength', 'sepalwidth', 'petallength', 'petalwidth', 'species'])
+
+# Get the data where the `species` column is `Iris-setosa` using advanced indexing
+data_filtered_pd = data_pd[data_pd['species'] == 'Iris-setosa']
+# Sort the values on the `petallength` column
+data_sorted_pd = data_filtered_pd.sort_values('petallength')
+# Get the values as an `ndarray`
+data_sorted_filtered = data_sorted_pd['petallength'].values
+
+print('Pandas')
+print(f'Overall: \t{data_sorted_filtered[-2]}')
+
+# Since the exercise uses the secnd largest unique value
+print(f'Unique: \t{np.unique(data_sorted_filtered)[-2]}')
