3ae0081e53d4cd249837cbf0e094e88e1c1af9e4
Christopher Sardegna <github@reagentx.net>
2018-03-25 16:02:02 -0700

    Print prettier arrays

--- scripts/47.py
+++ scripts/47.py
@@ -3,6 +3,7 @@ import numpy as np
 low = 10
 high = 30
 check = lambda a, l, h: print('Numbers are out of range.' if [x for x in a if x > h or x < l] else 'Numbers in range!')
+np.set_printoptions(precision=2)
 
 np.random.seed(100)
 a = np.random.uniform(1, 50, 20)
