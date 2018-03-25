910be733eec4e19d2acc18cfb1760ede65a727ff
Christopher Sardegna <github@reagentx.net>
2018-03-25 16:03:14 -0700

    Organize code

--- scripts/47.py
+++ scripts/47.py
@@ -1,11 +1,16 @@
 import numpy as np
 
+# Set some varaibles
 low = 10
 high = 30
+np.random.seed(100)
+
+# Create anonymous function to check our work, if the array is empty it is also `False`
 check = lambda a, l, h: print('Numbers are out of range.' if [x for x in a if x > h or x < l] else 'Numbers in range!')
+
+# Reduce precision for printing
 np.set_printoptions(precision=2)
 
-np.random.seed(100)
 a = np.random.uniform(1, 50, 20)
 print(a)
 check(a, low, high)
