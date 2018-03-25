f29d9e18f5bb332f6f4697d3d9d624efe22b3492
Christopher Sardegna <github@reagentx.net>
2018-03-24 20:50:57 -0700

    Add pandas example

--- scripts/45.py
+++ scripts/45.py
@@ -1,17 +1,25 @@
 import numpy as np
+import pandas as pd
 from collections import Counter
 
 
 url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
 
 # Get all columns as objects so we can understand the last column
-data = np.genfromtxt(url, delimiter=',', dtype='object')
+data = np.genfromtxt(url, delimiter=',', dtype='float')
 
 # Pure python
-print(Counter(data[..., 2]).most_common(1))
+print(f'Python:\t{Counter(data[..., 2]).most_common(1)[0]}')
 
 # NumPy
 # Using `return_counts=True` also returns the counts of the unique values
 vals, counts = np.unique(data[..., 2], return_counts=True)
 # `np.argmax(counts)` returns the index of the position that is the largest in `counts`
-print(f'[({vals[np.argmax(counts)]}, {max(counts)})]')
+print(f'NumPy:\t{vals[np.argmax(counts)]}, {max(counts)}')
+
+# Pandas
+data_pd = pd.read_csv(url, names=['sepallength', 'sepalwidth', 'petallength', 'petalwidth', 'species'])
+
+# Get the unique value counts
+uniques = data_pd['petallength'].value_counts()
+print(f'Pandas:\t{uniques.index[0]}, {uniques.iloc[0]}')
