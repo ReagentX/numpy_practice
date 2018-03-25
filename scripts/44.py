4883beff96ca35fc3b7b1d1d22b6278e608df046
Christopher Sardegna <github@reagentx.net>
2018-03-24 20:37:25 -0700

    Add pandas example

--- scripts/44.py
+++ scripts/44.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 
 url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
@@ -8,3 +9,10 @@ data = np.genfromtxt(url, delimiter=',', dtype='object')
 
 data_sorted = data[data[:, 0].argsort()]
 print(data_sorted[::25])
+
+# Pandas example
+data_pd = pd.read_csv(url, names=['sepallength', 'sepalwidth', 'petallength', 'petalwidth', 'species'])
+
+# Sort the data by the `sepallength` column
+data_sorted_pd = data_pd.sort_values('sepallength')
+print(data_sorted_pd[::25])
