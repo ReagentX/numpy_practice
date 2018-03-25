9629106cbdd4e7ffefa4424936967eccf0699727
Christopher Sardegna <github@reagentx.net>
2018-03-25 15:42:52 -0700

    Add Pandas example

--- scripts/46.py
+++ scripts/46.py
@@ -1,10 +1,12 @@
 import numpy as np
+import pandas as pd
 
 
 url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
 
 # Get all columns as floats since we ignore the last column
 data = np.genfromtxt(url, delimiter=',', dtype='object')
+print('NumPy:')
 
 # The actual datapoint without the position using advanced indexing
 data_filtered = data[data[..., 3].astype(float) > 1.0]
@@ -12,3 +14,10 @@ print(data_filtered[0])
 # The position, given by np.argwhere()
 data_position = np.argwhere(data[..., 3].astype(float) > 1.0)
 print(data_position[0])
+
+# Pandas Example
+data_pd = pd.read_csv(url, names=['sepallength', 'sepalwidth', 'petallength', 'petalwidth', 'species'])
+
+data_filtered = data_pd[data_pd['petalwidth'] > 1.0]
+print('\nPandas:')
+print(data_filtered.iloc[0])
