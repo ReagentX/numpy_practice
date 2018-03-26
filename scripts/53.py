8f1179bb8bb0d13e55d115a8a40164c7183cbbff
Christopher Sardegna <github@reagentx.net>
2018-03-25 20:17:11 -0700

    Clarify comments

--- scripts/53.py
+++ scripts/53.py
@@ -5,9 +5,10 @@ url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
 data = np.genfromtxt(url, delimiter=',', dtype='str', usecols=4)
 d_s = np.sort(np.random.choice(data, size=20))
 
-# Get unique values as a dict
-d_s_u = dict(zip(np.unique(d_s), np.arange(len(d_s))))
+# Get unique values as a dict so we can look them up
+make_dict = lambda x: dict(zip(np.unique(x), np.arange(len(x))))
 
-# Get the data as a new list of IDs
+# Get the data as a new list of IDs by looking up the values in the dict
+d_s_u = make_dict(d_s)
 r = [d_s_u[x] for x in d_s]
 print(r)
