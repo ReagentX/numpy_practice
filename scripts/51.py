3af6ba9c91629cf963d312de2449f14a58ca7c7a
Christopher Sardegna <github@reagentx.net>
2018-03-25 18:49:40 -0700

    Add new exercise

---
+++ scripts/51.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+# Generate the random array between two values and display it
+a = np.random.randint(1, 5, size=6)
+print(a)
+
+# Find the unique values
+u = np.unique(a)
+# Generate one-hot encodings for the unique values
+one_hot = np.eye(len(u))
+
+# Encode by iterating through the original array and pulling the respective one_hot using the index `np.where(u == x)`
+encoded = np.array([one_hot[np.where(u == x)][0] for x in a])
+print('NumPy:')
+print(encoded)
