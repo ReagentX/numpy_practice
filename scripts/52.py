947e7bf8c9c0bfd6d0d424434193ed84cd9de2b9
Christopher Sardegna <github@reagentx.net>
2018-03-25 20:09:58 -0700

    Comment debug code

--- scripts/52.py
+++ scripts/52.py
@@ -12,7 +12,7 @@ for i, v in enumerate(d_s, 0):
     # Append the counter variable
     r.append(c)
     # Debug
-    print(f'{c}\t{i}: {d_s[i]}, {max(i-1, 0)}: {d_s[max(i-1, 0)]}, {d_s[i] == d_s[max(i-1, 0)]}')
+    # print(f'{c}\t{i}: {d_s[i]}, {max(i-1, 0)}: {d_s[max(i-1, 0)]}, {d_s[i] == d_s[max(i-1, 0)]}')
     # If the current item is the same as the next one, add 1, else reset to 0
     if d_s[i] == d_s[min(i+1, len(d_s)-1)]:
         c += 1 
