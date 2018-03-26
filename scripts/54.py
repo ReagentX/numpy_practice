5b767101547c13dc907f66afff4f0e0bc1845c42
Christopher Sardegna <github@reagentx.net>
2018-03-25 20:31:59 -0700

    Add scipy example

--- scripts/54.py
+++ scripts/54.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.stats import rankdata
 
 
 np.random.seed(10)
@@ -7,5 +8,11 @@ print(list(a))
 
 # `argsort()` works by sorting the list and returning the indexes that should go in those respectve places
 # By calling it twice, we sort the indexes such that they align with the original numbers
-r = a.argsort().argsort()
-print(list(r))
+ranks = a.argsort().argsort()
+print(list(ranks))
+
+# Scipy example
+# This is 1-indexed, so we need to subtract 1 to make it zero indexed and cast as int
+# This also ignores duplucates, so 9 twice gets a tie rank of 4
+ranks_sp = (rankdata(a) - 1).astype(int)
+print(list(ranks_sp))
