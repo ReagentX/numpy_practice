91ab6bded87fd7badd9505afc395b4ebc1dd1450
Christopher Sardegna <github@reagentx.net>
2018-04-01 16:15:13 -0700

    Add comprehension example

--- scripts/56.py
+++ scripts/56.py
@@ -10,3 +10,11 @@ This is the better solution because it can also work for columns
 '''
 a_n = np.amax(a, axis=1)
 print(a_n)
+
+# Using dictionary comprehension (keep index)
+a_d = {i: max(a) for i, a in enumerate(a)}
+print(a_d)
+
+# Using list comprehension (ignore index)
+a_l = [max(i) for i in a]
+print(a_l)
