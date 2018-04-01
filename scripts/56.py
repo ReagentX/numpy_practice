089f42157e66c5e1e2635cfcdf085ba73cfee4c9
Christopher Sardegna <github@reagentx.net>
2018-04-01 16:15:36 -0700

    Fix comment

--- scripts/56.py
+++ scripts/56.py
@@ -4,7 +4,8 @@ import numpy as np
 a = np.random.randint(1, 20, [5, 3])
 print(a)
 
-'''Using NumPy
+'''
+Using NumPy
 We specify `axis=1` to operate in terms of rows and not columns
 This is the better solution because it can also work for columns
 '''
