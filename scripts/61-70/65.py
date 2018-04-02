d2a325b4058136791301db0308fd67a9663f3f12
Christopher Sardegna <github@reagentx.net>
2018-04-02 13:34:52 -0700

    Add exercise

---
+++ scripts/61-70/65.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+a = np.array([1, 2, 1, 1, 3, 4, 3, 1, 1, 2, 1, 1, 2])
+v = 5
+
+# `np.where()` generates a list of indeces where the criteria is met when args `x, y` are not passed
+b = np.where(a == 1)
+# Since `np.where` returns a list of tuples we need to access the first (only) item and pull the index we want (zero-indexed)
+print(b[0][v-a])
