d208b17a32152edfd02337539de87483a00b6e48
Christopher Sardegna <github@reagentx.net>
2018-04-01 21:36:02 -0700

    Add exercise

---
+++ scripts/58.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+a = np.random.randint(0, 5, 10)
+print(a)
+
+# Convert to a list so we can access the count() function
+a = list(a)
+
+'''
+Use comprehension to solve this
+By enumerating the list, we can index the list up to the current item to check for duplicates
+This way we fulfill the requirement of making the first occurance False
+'''
+b = np.array([True if a[:i+1].count(x) > 1 else False for i, x in enumerate(a)])
+print(b)
