ae340e88c69c0be5aad35512d053339f98dc541a
Christopher Sardegna <github@reagentx.net>
2018-04-02 14:07:23 -0700

    Add exercise

---
+++ scripts/61-70/67.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+a = np.random.randint(10, size=10)
+print(a)
+
+# Python example
+stride = 3  # Moving average includes `stride + 1` since arrays are zero indexed
+p_avg = [a[i:i + stride].sum()/(stride) for i, v in enumerate(a)]
+print(p_avg)
