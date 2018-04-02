77493cf98fac54560f7e594b6e46726f2fb19856
Christopher Sardegna <github@reagentx.net>
2018-04-02 13:08:28 -0700

    Add scipy example

--- scripts/61-70/62.py
+++ scripts/61-70/62.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.spatial import distance
 
 
 a = np.array([1,2,3,4,5])
@@ -6,4 +7,8 @@ b = np.array([4,5,6,7,8])
 
 # NumPy Example using Linear Algebra
 d_np = np.linalg.norm(a-b)
-print(f'{d_np:.3f}')
+print(f'NumPy:\n{d_np:.3f}')
+
+# Scipy example (only works with 1D arrays)
+d_sp = distance.euclidean(a, b)
+print(f'Scipy:\n{d_sp:.3f}')
