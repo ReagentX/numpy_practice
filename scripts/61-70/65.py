ea77ece37b53c15467e4e123ef18a1c3d649d13a
Christopher Sardegna <github@reagentx.net>
2018-04-02 13:36:18 -0700

    Add comprehension example

--- scripts/61-70/65.py
+++ scripts/61-70/65.py
@@ -7,4 +7,8 @@ v = 5
 # `np.where()` generates a list of indeces where the criteria is met when args `x, y` are not passed
 b = np.where(a == 1)
 # Since `np.where` returns a list of tuples we need to access the first (only) item and pull the index we want (zero-indexed)
-print(b[0][v-a])
+print(f'NumPy:\n{b[0][v-1]}')
+
+# Python Example
+c = [i for i, v in enumerate(a) if v == 1]
+print(f'Python:\n{c[v-1]}')
