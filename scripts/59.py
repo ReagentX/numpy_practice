ca0638fd0706aab691e48741fa1f9c40094725b9
Christopher Sardegna <github@reagentx.net>
2018-04-01 21:48:41 -0700

    Add exercise

---
+++ scripts/59.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+
+
+url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
+
+# Get all columns as objects, we will need to convert to do math
+data = np.genfromtxt(url, delimiter=',', dtype='object')
+print('NumPy:')
+
+# Loop through all of the unique values
+for i in np.unique(data[..., 4]):
+    # Use advanced indexing to pull column [1] from the rows where column 4 is i
+    print(f'{i}: \t{data[data[..., 4] == i, [1]].astype(float).mean():.3f}')
