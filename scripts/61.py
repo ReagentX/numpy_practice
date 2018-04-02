fdc62548799fc16dbf8048351cafa636e0ade47b
Christopher Sardegna <github@reagentx.net>
2018-04-01 22:52:40 -0700

    Add comprehension example

--- scripts/61.py
+++ scripts/61.py
@@ -8,3 +8,8 @@ print(a)
 # ~ is shorthand for a logical not in NumPy since we cannot do a normal `not` here
 b = a[~np.isnan(a)]
 print(b)
+
+# Using list comprehension 
+# Cast x as int for readability
+b_c = [int(x) for x in a if not np.isnan(x)]
+print(b_c)
