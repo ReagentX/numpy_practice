b831f908fe8b4d9112ece6996c1189e1e3e485a8
Christopher Sardegna <github@reagentx.net>
2018-04-02 14:09:09 -0700

    Add function example

--- scripts/61-70/67.py
+++ scripts/61-70/67.py
@@ -8,3 +8,10 @@ print(a)
 stride = 3  # Moving average includes `stride + 1` since arrays are zero indexed
 p_avg = [a[i:i + stride].sum()/(stride) for i, v in enumerate(a)]
 print(p_avg)
+
+# As a function
+def moving_average(a, s):
+    return [a[i:i + s].sum()/(s) for i, v in enumerate(a)]
+
+
+print(moving_average(a, 3))
